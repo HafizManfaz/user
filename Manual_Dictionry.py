1cb9f11d69584829df4232113023f8366af7f439
Hafiz Manfaz Ali <57103607+HafizManfaz@users.noreply.github.com>
2019-12-19 03:06:06 +0500

    Add files via upload

---
+++ Manual_Dictionry.py
@@ -0,0 +1,14 @@
+d1 = {}
+name = input("Enter Your Name :")
+d1["Name"] = name
+father_name = input("Enter Your Father Name :")
+d1["Father_Name"] = father_name
+city = input("Enter Your City :")
+d1["City"] = city
+cell = input("Enter Your Mobile Number :")
+d1["Mobile Number"] = cell
+d1["Skills"] = ["Python"]
+d1["Skills"] = d1["Skills"]+input("Enter Your Skills").split()
+print(d1)   
+
+
